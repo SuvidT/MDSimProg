cbfd35635825efe7673065b983232489e77f2d26
Suvid Thungathurti <suvidthungathurti@MacBookAir.attlocal.net>
2025-05-03 23:46:50 -0400

    initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+# --------------------
+# IMPORTS
+# --------------------
+from os import listdir, path
+
+# --------------------
+# CONSTANTS
+# --------------------
+TARGET_FOLDER = '/Users/suvidthungathurti/Documents/VS Code/MDSimProg'
+
+# --------------------
+# FUNCTIONS
+# --------------------
+def get_md_paths()
